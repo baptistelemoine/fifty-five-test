49b3f709a52fd5de615040ba18cb8a0b61d997d0
baptistelemoine <baptiste.lemoine@gmail.com>
2014-02-12 18:14:02 +0100

    design details view, add spinner

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -5,30 +5,40 @@ app.controllers.controller('HomeController', ['$scope', 'DataService', '$routePa
 	$scope.graphHeight = '350';
 	$scope.graphWidth = '800';
 	$scope.gap = 20;
-	$scope.divider = 4;
+	$scope.divider = 6;
 	$scope.nbCharts = 0;
 
 	$scope.items = [];
 
+	$scope.margin = 0;
+
+	$scope.type = $routeParams.type;
+
 	if($routeParams.type === 'daily'){
 		DataService.parse().then(function (result){
-			$scope.nbCharts = result.data.length;
-			$scope.chartWidth = ($scope.graphWidth / $scope.nbCharts) >> 2;
-			$scope.items = result.data;
+			$scope.dataBind(result);
 		});
 	}
 	else if($routeParams.type === 'weekly'){
 		$scope.gap = 10;
 		$scope.divider = 100;
 		DataService.parse('w').then(function (result){
-			$scope.nbCharts = result.data.length;
-			$scope.chartWidth = ($scope.graphWidth / $scope.nbCharts) >> 2;
-			$scope.items = result.data;
+			$scope.dataBind(result);
 		});
 	}
 
+	$scope.dataBind = function(result){
+		$scope.nbCharts = result.data.length;
+		$scope.chartWidth = ($scope.graphWidth / $scope.nbCharts) >> 2;
+		$scope.items = result.data;
+		$scope.settings = result.settings;
+		$scope.margin = ($scope.graphWidth-(($scope.chartWidth*2)+$scope.gap)*$scope.items.length) >> 1;
+	};
+
 	$scope.onItemClick = function(item, e){
 		e.preventDefault();
+		$scope.detailsActive = true;
+		$scope.detail = item;
 	};
 
 }]);
