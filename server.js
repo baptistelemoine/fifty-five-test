fb55a7fe171f6b7cef987c73ec6f133c9b5a86dc
baptistelemoine <baptiste.lemoine@gmail.com>
2014-02-12 11:42:03 +0100

    reformat api response

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 var express = require('express');
 var path = require('path');
+var http = require('http');
+var _ = require('underscore');
+var Q = require('q');
+
 var app = module.exports = express();
 
 app.configure(function () {
@@ -11,3 +15,43 @@ app.configure(function () {
 });
 
 app.listen(3000);
+
+//reformat response, will be more clean & easy on front end
+//localhost:3000/data?type=d for daily scores
+//localhost:3000/data?type=w for weekly scores
+//if no query : daily scores by default
+app.get('/data', function (request, response){
+	
+	http.get('http://demo55.apiary.io/data', function (res){
+		var output = '';
+		res.on('data', function (data){
+			output += data;
+		});
+		res.on('end', function(){
+
+			var type = request.query.type;
+			if(type === 'd') type = 'DAILY';
+			else if(type == 'w') type = 'WEEKLY';
+			else type = 'DAILY';
+
+			var result = JSON.parse(output);
+			var dates = result.data[type].dates;
+			var players = result.data[type].dataByMember;
+
+			var list = _.map(dates, function (val, i){
+				return {
+					'date':val,
+					'john':players.john.points[i],
+					'mike':players.mike.points[i]
+				};
+
+			});
+			list = _.filter(list, function (val){
+				return !_.isNull(val.john) || !_.isNull(val.mike);
+			});
+
+			response.json(_.extend({}, {'data':list}, {'settings':result.settings}));
+		});
+	});
+
+});
